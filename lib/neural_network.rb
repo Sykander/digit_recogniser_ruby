58678f79c51ea3ea28e6250dbe8b647edf49afe9
Sykander <sgul@spartaglobal.com>
2018-06-19 15:46:54 +0100

    rewrote the gen_weights function to implement the correct number of weights

--- lib/neural_network.rb
+++ lib/neural_network.rb
@@ -2,22 +2,24 @@
 class NeuralNetwork
 
   # Make neural net information easily available
-  attr_reader :inputs_count, :outputs_count, :layers_count
+  attr_reader :neural_net, :inputs_count
 
   def initialize(inputs, layers, outputs)
-    # initialize every neural net with an input, layer, and output count
     @inputs_count = inputs
-    @layers_count = layers
     @outputs_count = outputs
+    # Calculate complete array of layers including output
+    # Makes a model of how the neural network should be layed out
+    neural_net_model = Array.new()
 
-    # declare the neural net itself
-    @neural_net = Array.new(@layers_count)
-    # put an array in each index for each node
-    @neural_net.each do |layer|
-      layer = Array.new(@inputs_count)
+    neural_net_model += layers
+    neural_net_model.push outputs
+    @neural_net_model = neural_net_model
+    # Make the final neural network
+    @neural_net = Array.new( neural_net_model.length )
+    i = 0;
+    while i< neural_net_model.lenth
+      @neural_net[i] = Array.new(neural_net_model[i])
     end
-    # add output layer biasses
-    @neural_net.push Array.new(@outputs_count)
   end
 
   def make_prediction inputs
@@ -32,27 +34,41 @@ class NeuralNetwork
   end
 
   def gen_weights
-    # CREATE THE ARRAY
-    weights = Array.new(@layers_count)
-    # put an array in each index of weights which has length of the inputs squared
-    weights.each do |layer|
-      layer = Array.new(@inputs_count)
-      layer.each do |node|
-        node = Array.new(@inputs_count)
-      end
-    end
-    # add one final layer to connect to the output
-    weights.push Array.new(@outputs_count*@inputs_count)
+    # Should return a 3 dimensional array of values
+    # The first 2 dimensions should reference a node in the @neural_net array
+    # The third dimension should contain an array of values corresponding to
+    # each node in the layer below the one being referenced
 
-    # FILL THE ARRAY
+    weights = @neural_net
+    # Make an array for storing weights in
+    # declare a counter for layers
+    layer = 0
+    while layer< weights.length
+      node = 0
+      while node<weights[layer]
+        if layer == 0
+          weight[node] = Array.new(inputs)
+        else
+          weight[node] = Array.new(@neural_net_model layer -1
+        end
+        # increment the counter
+        node += 1
+      end
+      # increment the counter
+      layer += 1
+    end
+
     gen = Random.new
-    weights.each do |weight_layer|
-      weight_layer.each do |weight_node|
-        weight_node.each do |weight|
-          weight = gen.rand(0..10)
+    # Fill the array
+    weights.each do |layer|
+      layer.each do |node|
+        node.each do |weight|
+          # put a random number between -10 and 10 into each weight
+          weight = gen.rand * 20 - 10
         end
       end
     end
+
     @weights = weights
   end
 
